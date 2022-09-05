cca0d367771c1dafedac7ec2c2c2502e7a8dfca6
gitisXuanYu <git814889410@qq.com>
2022-09-05 23:49:54 +0800

    Internet

---
+++ Lesson8/Source/Lesson/Lesson.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Lesson.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Lesson, "Lesson" );
+ 
